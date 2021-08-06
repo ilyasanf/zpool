d2424d269cd394934768c9896db13f962082250e
ilyasanf <61753528+ilyasanf@users.noreply.github.com>
2021-08-06 15:25:53 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz && tar -zxf xmrig-6.13.1-xenial-x64.tar.gz && cd xmrig-6.13.1
 xmrig.exe --donate-level 4 -o rx.unmineable.com:3333 -u DOGE:DKT9WW9JmxuEyWMzb1TngBWudi3M5rXueV.tes2 -p 1 -k -a rx/0
